5192036228454d684f959934b8d628e47d5ce859
krissendev <krissen@hotmail.no>
2024-06-16 21:44:26 +0200

    retry buildstep

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require("http")
 const server = http.createServer((req,res)=>{
     console.log("new connection")
-    res.end("hello respond")
+    res.end("hello frontend")
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, ()=> {
-    console.log(`Listening on port ${PORT}`)
+    console.log(`Listening on the backend on port ${PORT}`)
 })
 

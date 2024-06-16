e7d5f6be024d8f7013fce079c1dda48fdd2c6e10
krissendev <krissen@hotmail.no>
2024-06-16 22:24:53 +0200

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http")
 const server = http.createServer((req,res)=>{
     console.log("new connection")
-    res.end("hello frontend")
+    res.end("hello frontend more text")
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, ()=> {

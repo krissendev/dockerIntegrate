28215985bd2fa210ee55cec669cacfe13950a611
krissendev <krissen@hotmail.no>
2024-07-27 00:38:26 +0200

    basic svelte

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,20 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+export default defineConfig(({ mode }) => ({
+  plugins: [svelte({
+  
+  //This section is for learning svlete build process, can be removed later
+  // enabling sourcemaps in development and production
+      compilerOptions: {
+        dev: mode === 'development' // This is for better debugging in development
+      }
+  })],
+  build: {
+    sourcemap: mode === 'development' || mode === 'production',  // Enable source maps in development
+    minify: mode !== 'development' // Disable minification in development if desired
+  },
+
   server: {
     port: 3030,
     host: '0.0.0.0'
@@ -12,4 +24,4 @@ export default defineConfig({
     port: 8080,
     host: '0.0.0.0'
   }
-})
+}))

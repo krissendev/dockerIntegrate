660a441d6fb45ccd4d7a43798d56ba4916f2fe6a
krissendev <krissen@hotmail.no>
2024-10-04 12:54:45 +0200

    pre resolving cherrypick 9b5365a

---
+++ src/Content.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { onMount } from 'svelte';
+    import lang_en from './lang-en.json'
+    import lang_no from './lang-no.json'
+    $: greeting = "placeholder"
+    
+    onMount(() => {
+        let language = navigator.language;
+        console.log(language);
+        if(language === "no" || language === "nb-NO" || language === "nn-NO"){
+            greeting = lang_no.greeting;
+        }
+        else if(language === "en" || language === "en-GB" || language === "en-US"){
+            greeting = lang_en.greeting;
+        }
+        else{
+            greeting =="not found"
+        }
+    });
+    
+</script>
+<h2>{greeting}</h2>

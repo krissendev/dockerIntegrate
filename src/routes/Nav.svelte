bda01f875fdb5756c656072fefbac64a289f32b5
krissendev <krissen@hotmail.no>
2025-01-16 17:24:52 +0100

    cookie consent layout wip

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -4,20 +4,22 @@
     import {onMount} from 'svelte';
     import {toggleBurger, initOnMount} from '$lib/layout/nav.js'
     import Darkmode from './Darkmode.svelte'
+    import SaveCookie from './SaveCookie.svelte'
+    import CookieConsent from './CookieConsent.svelte'
     let elMainMenu  ;
     let elLinks;
-    let elModal;
-    
+    let elMobileModal;
+    let elCookieModal;
 
     //nav resize depends on 'browser', which only is available after mount
     onMount(()=>{
-        initOnMount(elLinks, elMainMenu, elModal);
+        initOnMount(elLinks, elMainMenu, elMobileModal);
     })
     
 
 
     function handleToggle(){
-        toggleBurger (elLinks, elModal, elMainMenu);
+        toggleBurger (elLinks, elMobileModal, elMainMenu);
     }
 </script>
 
@@ -26,12 +28,17 @@
       <a href="#home">Home</a>
       <a href="#projects">Projects</a>
       <a href="#about">About</a>
-      <a href="#contact">Contact</a>
+      <a href="#contact">Contact</a>    
+      <div class="navDividerLine"></div><!-->Some divider-->
       <a href="/">Aa</a>
       <Darkmode/>
+      <SaveCookie/>
     </div>
 
-    <div class="modal" bind:this={elModal}></div><!--Modal-->
+    <div name="mobileModal" class="modal" bind:this={elMobileModal}></div><!--Mobile Modal-->
+    <div name="cookieConsentModal" class="modal" bind:this={elCookieModal}>
+        <CookieConsent/>
+    </div><!--Cookie Modal-->
     <button aria-hidden="true" class="burger" on:click={handleToggle} bind:this={elMainMenu}>
         <div></div><div></div>
         <div></div>
---
+++ src/routes/SaveCookie.svelte
@@ -0,0 +1,78 @@
+<script></script>
+<!--Modal for cookie conscent-->
+<div style="display:inline-block;">
+  <div class="switch-container">
+      <span class="label-text">Save Cookie</span>
+      <label class="switch">
+        <input type="checkbox">
+        <span class="slider"></span>
+      </label>
+  </div>
+</div>
+<style>
+  .switch-container {
+      flex-direction: column; /* Stacks flex items vertically */
+      display: flex;
+      align-items: center; /* Align text and switch vertically */
+      margin: 10px;
+  }
+  .label-text{
+      vertical-align:middle;
+  }
+   /* The switch - the box around the slider */
+  .switch {
+      vertical-align:middle;
+      position: relative;
+      display: inline-block;
+      width: 60px;
+      height: 34px;
+  }
+
+  /* Hide default HTML checkbox */
+  .switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+  }
+
+  /* The slider */
+  .slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  /* opacity:70%; */
+  -webkit-transition: .4s;
+  transition: .4s;
+  }
+
+  .slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: hsl(0, 0%, 100%);
+  /* opacity:70%; */
+  -webkit-transition: .4s;
+  transition: .4s;
+  }
+
+  input:checked + .slider {
+  background-color: #2196F3;
+  }
+
+  input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+  }
+
+  input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+  }
+</style>

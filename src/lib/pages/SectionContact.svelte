c12f85d1694d55a2c9839ab3ed732c0f6ccf7a90
krissendev <krissen@hotmail.no>
2025-04-17 16:38:47 +0200

    testing/debugging cloud sveltekit environment variables - exploring form fail 403 - action='dynamicpath' to match dynamic url

--- src/lib/pages/SectionContact.svelte
+++ src/lib/pages/SectionContact.svelte
@@ -1,26 +1,36 @@
 <script>
     import {langdataContact }           from '$store/store.js';
 
+    import { onMount }                      from 'svelte';
     // let result;
 	// let enhanceToggle = $state(false);
     let { data,form } = $props();
+
 
     ////TEMP Code /routes/api/redirectmail/+server.js - DELETE THIS LATER, only used for testing/learning purposes
     // async function fetchData() {
     //     const response = await fetch('/api/redirectmail');
     //     result = await response.text();
     // }
-    
+
     // $effect(() => {
     //     console.log('Form state in +page.svelte:', form);
     // });
+    let actionPath = $state(undefined);
+    onMount(()=>{
+        const currentPath = window.location.pathname + window.location.hash;
+        // $: lang = $page.params.lang;
+        actionPath = window.location.pathname + window.location.hash + "?/email";
+        console.log("SECTIONCONTACT currentURL:", currentPath, " & emailURL:", actionPath)
+    })
 </script>
 <div class="pageSection contactSvelte">
     <br>
     <h3 id="contact">This is Contact</h3>    
     <p>{$langdataContact}</p>
-    <form method="POST" action="?/email" 
-        use:enhance>
+    <!-- <form method="POST" action="/[lang]?/email" use:enhance={handleEnhance}></form> -->
+    <!-- <form method="POST" action=`${currentPath}?/email` use:enhance> -->
+    <form method="POST" action={actionPath} use:enhance>
     <!-- <form method="POST" action="?/email" 
     use:enhance={()=>{
         enhanceToggle = true;

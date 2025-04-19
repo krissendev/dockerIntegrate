84cac12fdc0a4eed57cba43c007fcf24092cfffa
krissendev <krissen@hotmail.no>
2025-04-19 15:20:48 +0200

    testing/debugging cloud sveltekit environment variables - exploring form fail 403 - configurations

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+//npm uninstall @sveltejs/adapter-auto
+//npm install @sveltejs/adapter-node@4.0.0 -D
+
+import adapter from '@sveltejs/adapter-node';
 import path from 'path'
-/** @type {import('@sveltejs/kit').Config} */
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			esmodules: true 
+		  }),
+		csrf: {
+			checkOrigin: true
+		},
 		alias:{
 			$store: path.resolve('./src/store'), 
 			$styles: path.resolve('./src/styles'),
